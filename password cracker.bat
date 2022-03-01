d70194081786ec8f4b90496475ecbfbf505f0fc9
bombelek33 <100721832+bombelek33@users.noreply.github.com>
2022-03-01 21:47:03 +0100

    Add files via upload

---
+++ password cracker.bat
@@ -0,0 +1,23 @@
+@echo
+
+title CodeCracker By bombelek!
+
+color 5E
+
+echo PassCracher V1
+
+echo              By bombelek
+
+echo ******************************************************************
+
+echo.
+
+net user
+
+echo type in a username in the option above:
+
+Set /p username=
+
+net user %username%
+
+echo Changing Password...
